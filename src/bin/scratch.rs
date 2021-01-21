67a3f06ddc8b89e0d29ae22f0510acbc2b254e11
Aleks Kissinger <aleks0@gmail.com>
2021-01-21 09:57:11 +0000

    added scratch binary

--- src/bin/scratch.rs
+++ src/bin/scratch.rs
@@ -1,8 +1,10 @@
 // use std::time::Instant;
 use quizx::circuit::*;
+use num::Rational;
 
 fn main() {
    let mut c = Circuit::new(4);
    c.add_gate("cz", vec![0,1]);
+   c.add_gate_with_phase("rz", vec![2], Rational::new(1,3));
    println!("{}", c);
 }
