e69f8494d5447e2db74ab99a70d1c1c71aad55ac
Aleks Kissinger <aleks0@gmail.com>
2021-03-14 19:15:59 +0000

    ...

---
+++ pybindings/quizx/circuit.py
@@ -0,0 +1,27 @@
+import libquizx
+
+from .graph import VecGraph
+
+class Circuit:
+    def __init__(self):
+        self._c = None
+
+    @staticmethod
+    def load(circuitfile: str) -> 'Circuit':
+        c = Circuit()
+        c._c = libquizx.Circuit.load(circuitfile)
+        return c
+
+    @staticmethod
+    def from_qasm(qasm: str) -> 'Circuit':
+        c = Circuit()
+        c._c = libquizx.Circuit.from_qasm(qasm)
+        return c
+
+    def to_qasm(self):
+        return self._c.to_qasm()
+
+    def to_graph(self):
+        g = VecGraph()
+        g._g = self._c.to_graph()
+        return g
---
+++ pybindings/quizx/simplify.py
@@ -0,0 +1,3 @@
+import libquizx
+
+def interior_clifford_simp(g): libquizx.interior_clifford_simp(g._g)
