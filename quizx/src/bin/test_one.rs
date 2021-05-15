c645bd5560a3abe89a1349e276639e2dcb7a0ce9
Aleks Kissinger <aleks0@gmail.com>
2021-05-15 16:18:30 +0100

    mod_adder... also bugged

--- quizx/src/bin/test_one.rs
+++ quizx/src/bin/test_one.rs
@@ -23,7 +23,7 @@ use std::time::Instant;
 use std::{thread, time};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let f = "../circuits/bug.qasm";
+    let f = "../circuits/large/mod_adder_1048576.qasm";
     let time = Instant::now();
     println!("{}", f);
     let c = Circuit::from_file(f).expect(&format!("circuit failed to parse: {}", f));
