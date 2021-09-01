31e064833c292fe4e8dfc032cf5a604a967af07c
Aleks Kissinger <aleks0@gmail.com>
2021-09-01 16:38:34 +0100

    updated scripts to match paper

--- scripts/add_estimates_hs.py
+++ scripts/add_estimates_hs.py
@@ -2,11 +2,12 @@
 
 import csv
 import sys
+from bigfloat import BigFloat, log10
 
 def terms(t):
-    count = 7.0 ** (t // 6)
+    count = BigFloat('7.0') ** (t // 6)
     t = t % 6
-    count *= 2.0 ** (t // 2)
+    count *= BigFloat('2.0') ** (t // 2)
     t = t % 2
     if t == 1: count *= 2.0
     return count
@@ -15,8 +16,12 @@ with open(sys.argv[1]) as f:
     r = csv.reader(f)
     data = list(r)
     # data = data[1:]
-    print('"q","ccz","seed","terms","time","tcounts","unsimp_terms","naive_terms"')
+    print('"q","ccz","seed","terms","time","tcounts","unsimp_terms","naive_terms","log10_unsimp_terms","log10_naive_terms"')
     for d in data:
-        d.append(str(int(sum(terms(int(t)) for t in d[5].split(',')))))
-        d.append(str(terms(int(d[1]) * 4 * 7)))
+        unsimp_terms = int(sum(terms(int(t)) for t in d[5].split(',')))
+        naive_terms = terms(int(d[1]) * 4 * 7)
+        d.append(str(unsimp_terms))
+        d.append(str(naive_terms))
+        d.append(str(log10(unsimp_terms)))
+        d.append(str(log10(naive_terms)))
         print('"' + '","'.join(d) + '"')
