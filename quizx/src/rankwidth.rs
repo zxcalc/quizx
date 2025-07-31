33aa82dd86cbf779429710223937b4e14add52f6
Aleks Kissinger <aleks0@gmail.com>
2025-07-31 10:05:44 +0100

    added citation

--- quizx/src/rankwidth.rs
+++ quizx/src/rankwidth.rs
@@ -1,4 +1,16 @@
-//! Uses simulated annealing to find good rank decompositions of a graph
+//! This module uses simulated annealing to find good rank decompositions of a graph
+//!
+//! This module provides an implementation of a simulated annealing algorithm
+//! to find good rank decompositions of a graph. It is based on the master's thesis
+//! of Florian Nouwt:
+//!
+//! "A simulated annealing method for computing rank-width". University of Utrecht, 2022.
+//! https://doi.org/20.500.12932/41566
+//!
+//! Some specifics are ported from Florian's C# implementation, which is available here:
+//!
+//! https://github.com/Gericom/RankWidthApproximate/
+//!
 pub mod annealer;
 pub mod decomp_tree;
 
